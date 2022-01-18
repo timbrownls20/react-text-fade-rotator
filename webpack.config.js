d46507ca6487803d58e52e217779a23fde8fbb34
timbrownls20 <timbrownls26@hotmail.com>
2022-01-18 16:32:30 +1000

    stash semi working version prior to rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,63 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  //template: "./src/index.html",
+  filename: "./index.html",
+});
+
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./index.tsx",
-  output: {
-    filename: "index.js",
-  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "ts-loader"],
+        use: ["ts-loader"],
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.tsx$/,
-        use: ["source-map-loader"],
-        enforce: "pre",
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: ["file-loader"],
-      },
-    ],
+    ]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", "css"],
-  }
+    extensions: [".tsx", ".ts", ".js", ".css"],
+  },
+  plugins: [htmlPlugin],
 };
+
+
+// module.exports = {
+//   mode: "development",
+//   entry: "./index.tsx",
+//   output: {
+//     filename: "index.js",
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         exclude: /node_modules/,
+//         use: ["babel-loader", "ts-loader"],
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.tsx$/,
+//         use: ["source-map-loader"],
+//         enforce: "pre",
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif)$/i,
+//         use: ["file-loader"],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [".tsx", ".ts", ".js", "css"],
+//   }
+// };
